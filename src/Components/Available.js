cc0120ccefe0c97017db29faf92cabc8c4410075
MarianRusoiu99 <marian.rusoiu99@e-uvt.ro>
2023-06-26 20:13:16 +0300

    fix

--- src/Components/Available.js
+++ src/Components/Available.js
@@ -1,5 +1,5 @@
 import React, {useEffect,useRef} from 'react'
-
+import { motion } from 'framer-motion';
 function Available() {
 
 const text = useRef()
@@ -7,16 +7,25 @@ const text = useRef()
 useEffect(()=>{
     console.log(text);
     text.current.innerHTML = text.current.innerHTML.split("").map((char,i)=>
-        `<span style="transform:rotate(${i * 8.1}deg)">${char}</span>`
+        `<span style="transform:rotate(${i * 19}deg)">${char}</span>`
         ).join('')
 },[])
 
   return (
     <div className='circle'>
-        <div className='greenDot'></div>	
-        <div className='textC'>
-            <p ref={text}> AVAILABLE FOR WORK- AVAILABLE FOR WORK- </p>
-        </div>
+        <motion.div
+        // initial={{opacity:0}}
+        animate={{opacity:[0,1,0]}}
+        transition={{
+          type:"spring",
+          duration: 2,
+          ease: "easeInOut",
+          repeat: Infinity,
+          repeatDelay: 1
+        }}
+        className='greenDot'></motion.div>	
+        <p ref={text} className='textC'> AVAILABLE-FOR-WORK- </p>
+        
     </div>
   )
 }
